5d181104c1a99a528c548d61c86f3207267e0495
dilip baghel <dilip.18baghel@gmail.com>
2023-11-06 00:21:56 +0530

    how DOM tree is actually created in the React'

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", className: "head" },
+//   "Hello world from React"
+// );
+
+/*
+how the DOM tree id creating inside the React
+
+<div id="parent">
+    <div id='child'>
+        <h1>Hello i'm inside the child</h1>
+    </div>
+</div>
+
+*/
+
 const heading = React.createElement(
-  "h1",
-  { id: "heading", className: "head" },
-  "Hello world from React"
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "Hello i'm inside the child")
+  )
 );
 
-console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
