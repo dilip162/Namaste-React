99887c98df1611468049b64a8616c25eb50dcf36
dilip baghel <dilip.18baghel@gmail.com>
2023-11-06 17:03:23 +0530

    Learn about parcel, bundlers, dependencies and lot more

--- App.js
+++ App.js
@@ -1,25 +1,11 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", className: "head" },
-//   "Hello world from React"
-// );
-
-/*
-how the DOM tree id creating inside the React
-
-<div id="parent">
-    <div id='child'>
-        <h1>Hello i'm inside the child</h1>
-    </div>
-</div>
-
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const heading = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello i'm inside the child"),
+    React.createElement("h1", {}, "Hello i'm inside the child "),
     React.createElement("h1", {}, "Hello, I am the second heading"),
     React.createElement("h1", {}, "Hello, I am the third heading"),
   ])
